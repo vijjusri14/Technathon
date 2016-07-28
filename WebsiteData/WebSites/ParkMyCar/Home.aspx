90bd59fb122e9b0adf508a556c290f8f49b3e5d3
vijjusri14 <vijjusri14@gmail.com>
2016-07-28 14:56:03 +0530

    Add files via upload
    Website Commit

---
+++ WebsiteData/WebSites/ParkMyCar/Home.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Home.aspx.cs" Inherits="Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>ParkMyCar</title>
+    <link rel="icon" href="Images/iconCar.png" type="image/x-icon"/>
+    <link rel="stylesheet" href="StyleSheets/Home.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div id="header">
+        <h1>Park My Car<img id="iconCar" src="Images/iconCar.png" height="60" width="60"/></h1>
+        <p>Now check your slot with a swipe!!</p>
+    </div>
+        <asp:ImageButton ID="imgbtnCheck" runat="server" Height="50px" ImageUrl="~/Images/iconCar.png" OnClick="imgbtnCheck_Click" style="z-index: 1; left: 627px; top: 245px; position: absolute" ToolTip="Click for Check!!" />
+        <div class="selectionbar">
+            <asp:Label ID="lblCountry" runat="server" Text="Country:" position="absolute" style="z-index: 1; left: 11px; top: 10px; position: absolute; right: 1016px;"></asp:Label>
+            <asp:DropDownList ID="dropCountry" runat="server" position="absolute" style="z-index: 1; left: 73px; top: 10px; position: absolute;width:77px;height:22px; right: 740px;" AutoPostBack="True" OnSelectedIndexChanged="dropCountry_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:Label ID="lblCity" runat="server" Text="City:" position="absolute" style="z-index: 1; left: 230px; top: 10px; position: absolute;"></asp:Label>
+            <asp:DropDownList ID="dropCity" runat="server" position="absolute" style="z-index: 1; left: 270px; top: 10px; position: absolute;width:77px;height:22px; right: 733px;" AutoPostBack="True" OnSelectedIndexChanged="dropCity_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:Label ID="lblBuilding" runat="server" Text="Building:" position="absolute" style="z-index: 1; left: 456px; top: 10px; position: absolute;"></asp:Label>
+            <asp:DropDownList ID="dropBuilding" runat="server"  position="absolute" style="z-index: 1; left: 516px; top: 10px; position: absolute;width:77px;" AutoPostBack="True" OnSelectedIndexChanged="dropBuilding_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:Label ID="lblTower" runat="server" Text="Tower:" position="absolute" style="z-index: 1; left: 706px; top: 10px; position: absolute;"></asp:Label>
+            <asp:DropDownList ID="dropTower" runat="server" position="absolute" style="z-index: 1; left: 758px; top: 10px; position: absolute;width:77px;height:22px;" AutoPostBack="True" OnSelectedIndexChanged="dropTower_SelectedIndexChanged">
+            </asp:DropDownList>
+            <asp:Label ID="lblfloor" runat="server" Text="Floor:" position="absolute" style="z-index: 1; left: 930px; top: 10px; position: absolute;"></asp:Label>
+            <asp:DropDownList ID="dropfloor" runat="server" position="absolute" style="z-index: 1; left: 970px; top: 10px; position: absolute;width:77px;height:22px;" AutoPostBack="True" >
+            </asp:DropDownList>
+        </div>
+        <div id="slotmap" class="slotmap" runat="server">
+            <div class="slot1" style="height: 100px; width: 100px; z-index: 1; left: 200px; top: 20px; position: absolute;">
+                <img id="a1"  src='Images/AvailableTop.png' style="height: 100px; z-index: 1; left: 0px; top: 0px; position: absolute;"   runat="server" />               
+                <img id="na1"  src='Images/NotAvailableTop.png' style="height: 100px; z-index: 1; left: 0px; top: 1px; position: absolute;" runat="server"/>
+            </div>
+            
+
+            <div class="slot2" style="height: 100px; width: 100px; z-index: 1; right: 200px; top: 20px; position: absolute;">
+               <img  id="a2"  runat="server" src='Images/AvailableTop.png' style="height: 100px; z-index: 1; left: 0px; top: 0px; position: absolute;" />
+                <img  id="na2" runat="server" src='Images/NotAvailableTop.png' style="height: 100px; z-index: 1; left: 0px; top: 1px; position: absolute;" />
+            </div>
+
+
+                 <div class="slot3" style="height: 100px; width: 100px; z-index: 1; left: 200px; bottom: 20px; position: absolute;">
+               <img  id="a3" runat="server" src='Images/AvailableDown.png' style="height: 100px; z-index: 1; left: 0px; top: 0px; position: absolute;" />
+                <img  id="na3" runat="server" src='Images/NotAvailableDown.png' style="height: 100px; z-index: 1; left: 0px; top: 1px; position: absolute;" />
+            </div>
+
+            <div class="slot4" style="height: 100px; width: 100px; z-index: 1; right: 200px; bottom: 20px; position: absolute;">
+               <img  id="a4" runat="server" src='Images/AvailableDown.png' style="height: 100px; z-index: 1; left: 0px; top: 0px; position: absolute;" />
+                <img id="na4" runat="server" src='Images/NotAvailableDown.png' style="height: 100px; z-index: 1; left: 0px; top: 1px; position: absolute;" />
+            </div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebsiteData/WebSites/ParkMyCar/Home.aspx.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+public partial class Home : System.Web.UI.Page
+{
+    string strConnection = ConfigurationManager.ConnectionStrings["parkmyslotConnectionString"].ConnectionString;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            countrybind();
+            citybind();
+            buildingbind();
+            towerbind();
+            floorbind();
+            slotmap.Visible = false;
+        }
+    }
+
+    protected void countrybind()
+    {
+        MySqlConnection con = new MySqlConnection(strConnection);
+        string query = "SELECT DISTINCT COUNTRY_CODE, COUNTRY_DESC FROM tower ORDER BY COUNTRY_DESC";
+        con.Open();
+        MySqlDataAdapter adpt = new MySqlDataAdapter(query, con);
+        DataTable dt = new DataTable();
+        adpt.Fill(dt);
+        dropCountry.DataSource = dt;
+        dropCountry.DataBind();
+        dropCountry.DataTextField = "COUNTRY_DESC";
+        dropCountry.DataValueField = "COUNTRY_CODE";
+        dropCountry.DataBind();
+        dropCountry.Items.Insert(0, "--Select--");
+        con.Close();
+    }
+
+    protected void citybind()
+    {
+        MySqlConnection con = new MySqlConnection(strConnection);
+        string query = "SELECT DISTINCT CITY_CODE, CITY_DESC FROM tower WHERE COUNTRY_CODE='" + dropCountry.SelectedValue + "' ORDER BY CITY_DESC";
+        con.Open();
+        MySqlDataAdapter adpt = new MySqlDataAdapter(query, con);
+        DataTable dt = new DataTable();
+        adpt.Fill(dt);
+        dropCity.DataSource = dt;
+        dropCity.DataBind();
+        dropCity.DataTextField = "CITY_DESC";
+        dropCity.DataValueField = "CITY_CODE";
+        dropCity.DataBind();
+        dropCity.Items.Insert(0, "--Select--");
+        con.Close();
+    }
+    protected void buildingbind()
+    {
+        MySqlConnection con = new MySqlConnection(strConnection);
+        string query = "SELECT DISTINCT BUILDING_CODE, BUILDING_DESC FROM tower WHERE CITY_CODE='" + dropCity.SelectedValue + "'  ORDER BY BUILDING_DESC";
+        con.Open();
+        MySqlDataAdapter adpt = new MySqlDataAdapter(query, con);
+        DataTable dt = new DataTable();
+        adpt.Fill(dt);
+        dropBuilding.DataSource = dt;
+        dropBuilding.DataBind();
+        dropBuilding.DataTextField = "BUILDING_DESC";
+        dropBuilding.DataValueField ="BUILDING_CODE";
+        dropBuilding.DataBind();
+        dropBuilding.Items.Insert(0, "--Select--");
+        con.Close();
+    }
+    protected void towerbind()
+    {
+        MySqlConnection con = new MySqlConnection(strConnection);
+        string query = "SELECT TOWER_CODE, TOWER_DESC FROM tower WHERE BUILDING_CODE='"+dropBuilding.SelectedValue+"' ORDER BY TOWER_DESC";
+        con.Open();
+        MySqlDataAdapter adpt = new MySqlDataAdapter(query, con);
+        DataTable dt = new DataTable();
+        adpt.Fill(dt);
+        dropTower.DataSource = dt;
+        dropTower.DataBind();
+        dropTower.DataTextField = "TOWER_DESC";
+        dropTower.DataValueField = "TOWER_CODE";
+        dropTower.DataBind();
+        dropTower.Items.Insert(0, "--Select--");
+        con.Close();
+    }
+
+    protected void floorbind()
+    {
+        MySqlConnection con = new MySqlConnection(strConnection);
+        string query = "SELECT distinct FLOOR_CODE, FLOOR_DESC FROM  tower as t JOIN slot as s on t.TOWER_CODE=s.TOWER_CODE WHERE s.TOWER_CODE='"+dropTower.SelectedValue +"'ORDER BY FLOOR_CODE";
+        con.Open();
+        MySqlDataAdapter adpt = new MySqlDataAdapter(query, con);
+        DataTable dt = new DataTable();
+        adpt.Fill(dt);
+        dropfloor.DataSource = dt;
+        dropfloor.DataBind();
+        dropfloor.DataTextField = "FLOOR_DESC";
+        dropfloor.DataValueField = "FLOOR_CODE";
+        dropfloor.DataBind();
+        dropfloor.Items.Insert(0, "--Select--");
+        con.Close();
+    }
+
+    protected void dropCountry_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        citybind();
+    }
+
+    protected void dropCity_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        buildingbind();
+    }
+
+    protected void dropBuilding_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        towerbind();
+    }
+
+    protected void imgbtnCheck_Click(object sender, ImageClickEventArgs e)
+    {
+        if (dropCountry.SelectedValue == "--Select--" || dropCity.SelectedValue == "--Select--" || dropBuilding.SelectedValue == "--Select--" || dropTower.SelectedValue == "--Select--" || dropfloor.SelectedValue == "--Select--")
+        {
+            Response.Write("<script>alert('Please Select a valid entry')</script>");
+        }
+        else
+        {
+            map();
+        }
+            
+    }
+
+    protected void dropTower_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        floorbind();
+    }
+
+    protected void map()
+    {
+        
+
+        MySqlConnection con = new MySqlConnection(strConnection);
+        string query = "SELECT SLOT_CODE, AVAILABLE FROM tower as t join slot  as s on t.TOWER_CODE=s.TOWER_CODE WHERE s.TOWER_CODE=" + dropTower.SelectedValue + " ORDER BY SLOT_CODE";
+        con.Open();
+        MySqlDataAdapter adpt = new MySqlDataAdapter(query, con);
+        DataTable dt = new DataTable();
+        adpt.Fill(dt);
+        con.Close();
+        
+        for (int i = 0; i < dt.Rows.Count; i++)
+        {
+            
+            if (dt.Rows[i][1].ToString() == "YES")
+            {
+                string idna = "na" + i;
+                Control mycontrol = FindControl(idna);
+                if (mycontrol != null)
+                {
+                    mycontrol.Visible = false;
+                   
+                }
+            }
+
+        }
+        slotmap.Visible = true;
+    }
+}
